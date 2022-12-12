e8e4fa547b1a9b5c0be8ccb85d37827be0d8fcd3
MGH <rudgus1004@gmail.com>
2022-12-12 11:18:22 +0900

    사이드바 조정

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -1,7 +1,7 @@
 import { Route, Switch , NavLink} from 'react-router-dom';
 import styled from "styled-components";
 import imgFile from '../../assets/logo192.png';
-import React,{useState, useCallback,useRef } from 'react'; 
+import React,{useEffect,useState, useCallback,useRef } from 'react'; 
 import {MENU_LIST,CONTSTURL} from "../../vo/menuVo";
 //border-right: 1px solid #e0e0e0;
 const Side = styled.div`
@@ -9,8 +9,8 @@ const Side = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 200%;
-  height : 100%;
+  width: 15%;
+  height : 90%;
 `;
 const Profile = styled.img.attrs({
   src : `${imgFile}`
@@ -39,19 +39,60 @@ const Sidebar = (props:MENU_LIST) =>{
   
   let ref2e = useRef<any>(null);
   let moveBoolean : Boolean = false;
+  useEffect(()=>{
+    document.addEventListener('mouseup',mouseUp);
+  });
+
+  let moveCheck:string = "right";
+  const transitionPoint = () : number =>{
+    let strPx:string = ref2e.current.style.transform;
+    let split : string[] = strPx.split("(");
+    let numPx:number = Number(split[1].replace(/px\)/,""));
+    if(numPx == -1400){
+      moveCheck = "right";
+    }else{
+      moveCheck = "left";
+    }
+    return numPx;
+  }
+
   const mouseUp = () :void =>{
     moveBoolean = false;
+    console.log(ref2e.current);
+    let numPx : number = transitionPoint();
+    ref2e.current.style.transition = "transform 500ms";
+    if(numPx > -800){
+      ref2e.current.style.transform = "translateX(0px)";
+    }else{
+      ref2e.current.style.transform = "translateX(-1400px)";
+    }
+    
   }
 
+  let screenx = 0;
+  let defaultWidth:number = 0;
   const mouseDown = (e:any) :void =>{
+    screenx = e.screenX;
     moveBoolean = true;
+    ref2e.current.style.transition = "transform 0ms";
+    defaultWidth = transitionPoint();
+
   }
 
-  let defaultWidth:number = 20;
   const moveListener = (e:any) :void =>{
+    ref2e.current.style.transition = "transform 0ms";
     if(moveBoolean){
-      defaultWidth += 0.4;
-    //  ref2e.current.style.width = defaultWidth +"vw";
+      if(moveCheck === "right"){
+        defaultWidth = -1400;
+      }else{
+        defaultWidth = 0;
+      }
+      defaultWidth += e.screenX-screenx;
+
+      if(defaultWidth >= -1) {
+        return;
+      }
+      ref2e.current.style.transform = "translateX("+ defaultWidth+ "px)";
     }else{
 
     }
@@ -59,7 +100,7 @@ const Sidebar = (props:MENU_LIST) =>{
   }
 
   return (
-  <div className='sidebar sideT' onMouseMove={(e) => moveListener(e)} onMouseDown={(e)=> mouseDown(e)} onMouseUp={() => mouseUp() } ref={ref2e}>
+  <div className='sidebar sideT' style={{transform:"translateX(-1400px)"}} onMouseMove={(e) => moveListener(e)} onMouseDown={(e)=> mouseDown(e)} ref={ref2e}>
   <Side className='SideSlide'>
     <Menu>
     <Profile/>
--- src/const/commonConst.ts
+++ src/const/commonConst.ts
@@ -2,8 +2,8 @@ import {CONTSTURL} from "../vo/menuVo";
 
 export const menus : CONTSTURL[] =  
 [ 
-    { name : "Home" , path: "/"},
-    { name: "프로필", path: "/profile" },
-    { name: "이력", path: "/history" },
-    { name: "잡동", path: "/setting"}
+    { name : "Home" , path: "/mgh"},
+    { name: "프로필", path: "/mgh/profile" },
+    { name: "이력", path: "/mgh/history" },
+    { name: "잡동", path: "/mgh/setting"}
 ]
