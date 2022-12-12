8dd4e64fecacce4d67f63a5ee257e2225002de2c
MGH <rudgus1004@gmail.com>
2022-12-12 15:08:47 +0900

    사이드바 커밋

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -42,14 +42,25 @@ const Sidebar = (props:MENU_LIST) =>{
   useEffect(()=>{
     document.addEventListener('mouseup',mouseUp);
     document.addEventListener('mousemove',moveListener);
+    window.addEventListener('resize',ResizeEnvet);
   });
+
+  // 초기 로드 시 설정
+  let defaulSize = (2133 - window.innerWidth)+ -1735;
+  const ResizeEnvet = () =>{
+    let numPx = transitionPoint();
+    defaulSize = (2133 - window.innerWidth)+ -1735;
+    if(numPx != 0){
+      ref2e.current.style.transform = "translateX("+defaulSize+"px)";
+    }
+  }
 
   let moveCheck:string = "right"; // 사이드바 오른쪽 왼쪽 움직일 기준 변수
   const transitionPoint = () : number =>{ //현재 사이드바 위치 추적 하는 함수
     let strPx:string = ref2e.current.style.transform;
     let split : string[] = strPx.split("(");
     let numPx:number = Number(split[1].replace(/px\)/,""));
-    if(numPx == -1735){
+    if(numPx == defaulSize){
       // 왼쪽으로 접혀 있으면 오른쪽 기준
       moveCheck = "right";
     }else{
@@ -64,18 +75,18 @@ const Sidebar = (props:MENU_LIST) =>{
     moveBoolean = false;
     let numPx : number = transitionPoint();
     ref2e.current.style.transition = "transform 500ms";
-    if(numPx > -800){
+    if(numPx > defaulSize/1.5){
       // 반 이상 넘어 오면 전부 펼치기
       ref2e.current.style.transform = "translateX(0px)";
     }else{
       // 반 미만 으로 오면 원래 상태로 복귀
-      ref2e.current.style.transform = "translateX(-1735px)";
+      ref2e.current.style.transform = "translateX("+defaulSize+"px)";
     }
 
   }
 
   let screenx : number = 0; // 사이드바 클릭 시점 의 마우스 위치 변수
-  let defaultWidth : number = 0; // 사이드바 펼칠시 기준이 되는 변수
+  let defaultWidth : number = 0; // 사이드바 펼칠때 기준이 되는 변수
   const mouseDown = (e:any) :void =>{
     screenx = e.screenX;
     moveBoolean = true;
@@ -84,18 +95,19 @@ const Sidebar = (props:MENU_LIST) =>{
 
   }
 
-  const moveListener = (e:any) :void =>{ // 사이드바 움직일 시 함수
+  const moveListener = (e:any) :void =>{ // 사이드바 드래그 함수
     if(moveBoolean){
       if(moveCheck === "right"){
-        defaultWidth = -1735;
+        defaultWidth = defaulSize;
       }else{
         defaultWidth = 0;
       }
-      defaultWidth += e.screenX-screenx; // 오른쪽 기준일시 -1735 += (현재 마우스 위치 - 클릭 했을때 시점의 마우스 위치);
+      defaultWidth += e.screenX-screenx; // 오른쪽 기준일시 defaulSize += (현재 마우스 위치 - 클릭 했을때 시점의 마우스 위치);
 
       if(defaultWidth >= -1) {
         return;
       }
+
       ref2e.current.style.transform = "translateX("+ defaultWidth+ "px)";
     }else{
 
@@ -104,14 +116,14 @@ const Sidebar = (props:MENU_LIST) =>{
   }
 
   return (
-  <div className='sidebar sideT' style={{transform:"translateX(-1735px)"}} onMouseMove={(e) => moveListener(e)} onMouseDown={(e)=> mouseDown(e)} ref={ref2e}>
-  <Side className='SideSlide notdrag'>
+  <div className='sidebar sideT' style={{transform:"translateX("+defaulSize+"px)"}} onMouseMove={(e) => moveListener(e)} onMouseDown={(e)=> mouseDown(e)} ref={ref2e}>
+  <Side className='SideSlide Notdrag'>
     <Menu>
     <Profile/>
       {
         menus.map((menu,index)=>{
           return(
-            <NavLink exact className='notdrag' style={{color:"white",textDecoration:"none" , marginBottom:"15px"}} to={menu.path} key={index} activeStyle={{color:"red"}}>
+            <NavLink exact className='Notdrag' style={{color:"white",textDecoration:"none" , marginBottom:"15px"}} to={menu.path} key={index} activeStyle={{color:"red"}}>
               {menu.name}
             </NavLink>
           );
