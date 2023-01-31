ad98b06c69e86d726ff25a793d9dff7ef5bd6fa7
MGH <rudgus1004@gmail.com>
2023-01-31 10:19:11 +0900

    커밋

--- src/components/center/About.tsx
+++ src/components/center/About.tsx
@@ -1,4 +1,21 @@
 import {CenterChildrenDiv, CenterDiv} from "../../const/StyledConst";
+import styled from "styled-components";
+import imgFile from '../../assets/imgs/react.png';
+const Profile = styled.img.attrs({
+    src : `${imgFile}`
+  })`
+    width: 150px;
+    height: 150px;
+    border-radius: 100%;
+    margin-bottom : 20px;
+    margin-left: 5%;
+    margin-top: 3%;
+    background-color: #282c34;
+  `;
+const Info = styled.div`
+    width: 100%;
+    display: inline-flex;
+`
 
 const About = () : JSX.Element => {
 
@@ -7,135 +24,11 @@ const About = () : JSX.Element => {
             <CenterChildrenDiv>
                 <h1 className="title">About</h1>
                 <div className="hr"></div>
-                진짜 너무 졸린데?
-                <div>따라라라</div>
-                <div>
-                오키오키
-                </div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>스크롤 테스트</div>
-                <div>마지막 글입니다.</div>
+                <Info>
+                    <Profile/>
+                    <h2 style={{marginLeft:"3%",fontSize:"30px",marginTop:"3%"}}>민경현</h2>
+                </Info>
+
             </CenterChildrenDiv>
         </CenterDiv>
     
